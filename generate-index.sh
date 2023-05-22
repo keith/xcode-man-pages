027a1aae9e5366b3c9c86b1f6c04c819d7d61b3e
Mingye Wang <arthur200126@gmail.com>
2023-05-23 01:30:42 +0800

    Sectioned index with columns (#3)
    The hope is that people spend less time scrolling through .3pm

--- generate-index.sh
+++ generate-index.sh
@@ -7,6 +7,12 @@ header="<!DOCTYPE html>
 <html>
 <head>
   <title>Xcode's man pages</title>
+  <style>
+    body { font-family: sans-serif; }
+    h1, h2, h3, h4, h5, h6 { font-family: serif; }
+    .col ul { margin-top: 0; } 
+    .col { column-width: 20em; overflow-wrap: anywhere; hyphens: auto; } 
+  </style>
 </head>
 <body>
 <h1>Xcode $version</h1>
@@ -17,12 +23,41 @@ footer="</body>"
 output=index.html
 echo "$header" > "$output"
 
-for file in $(find . -name "*.html" -type f | sort)
+rm -r _tmp &>/dev/null || true
+mkdir -p _tmp
+
+for file in *.html
 do
-  name="${file:2}"
-  name="${name%.*}"
-  echo "- <a href='$file'>$name</a><br>" >> "$output"
+  [[ -f $file ]] || continue
+  name="${file%.html}"
+  section="${name##*.}"
+  printf "<li><a href='%s'>%s</a>\\n" "$file" "$name" >> "_tmp/$section"
 done
+rm _tmp/index
+
+# Emit TOC
+{
+  printf '<ul>\n'
+  for part in _tmp/*
+  do
+    section="${part##_tmp/}"
+    printf '<li><a href="#s.%s">Section %s</a>\n' "$section" "$section"
+  done
+  printf '</ul>\n'
+} >> "$output"
+
+# Emit sections
+for part in _tmp/*
+do
+  {
+    section="${part##_tmp/}"
+    printf '<h2 id="s.%s">Section %s</h2>\n' "$section" "$section"
+    printf '<div class="col"><ul>\n'
+    cat -- "$part"
+    printf '</div></ul>\n'
+  } >> "$output"
+done
+rm -r _tmp
 
 echo "$footer" >> "$output"
 ../add_custom_css.py "$output"
